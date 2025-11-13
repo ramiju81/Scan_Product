ef1668a62ff946b389e455ef2bdb8be40f51d8b9
Julian Ramirez <juliram81@gmail.com>
2025-11-13 12:33:50 -0500

    Update supabaseClient.js

--- static/js/supabaseClient.js
+++ static/js/supabaseClient.js
@@ -1,8 +1,8 @@
 // static/js/supabaseClient.js
 // Reemplaza estos valores por los de tu proyecto Supabase
 
-const SUPABASE_URL = "https://TU-PROYECTO.supabase.co";
-const SUPABASE_ANON_KEY = "TU-CLAVE-ANON";
+const SUPABASE_URL = "https://xhyucxumsdqgtpnwdqdu.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhoeXVjeHVtc2RxZ3RwbndkcWR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk4ODE3MzYsImV4cCI6MjA3NTQ1NzczNn0.QxrspFLu_S3OC9G7AP9eKu17JztVS22s0BGaE4pTlkU";
 
 window.supabase = window.supabase || {};
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
